7b889ba9110408b85ce8a6d75d4fdb0f406720c2
JeetPandey <dev.jeetpandey25@outlook.com>
2026-01-28 23:20:41 +0530

    initial setup, model created and registered in admin

---
+++ Todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField
+    description = models.TextField(default=False)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+    
